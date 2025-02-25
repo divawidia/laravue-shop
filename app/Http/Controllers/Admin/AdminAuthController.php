8275f4d14cc67faf313a82c90d211b79d73e724e
divawidia <wiartha2001@gmail.com>
2025-02-25 00:00:00 +0000

    feat (AdminAuthController): add login methods

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AdminAuthController extends Controller
@@ -12,4 +13,12 @@ class AdminAuthController extends Controller
     {
         return Inertia::render('Admin/Auth/Login');
     }
-}
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'isAdmin' => true])) {
+            return redirect()->route('admin.dashboard');
+        }
+
+        return redirect()->route('admin.login')->with('error', 'Invalid email or password');
+    }
