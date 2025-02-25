47ac0d26f6ea83a2838343068d71202f36a39b39
divawidia <wiartha2001@gmail.com>
2025-02-25 00:00:00 +0000

    feat (db migration): add attribute of create products table

--- database/migrations/2025_04_01_004244_create_products_table.php
+++ database/migrations/2025_04_01_004244_create_products_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->integer('quantity')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->boolean('inStock')->default(false);
+            $table->boolean('published')->default(false);
+            $table->text('description')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('deleted_by')->nullable()->constrained()->onDelete('set null');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
