0fe16bbfca6d309cf6be66c965f6e39109fe52b8
divawidia <wiartha2001@gmail.com>
2025-02-25 00:00:00 +0000

    feat (web routes): import AdminAuthController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
