51effa882a48d00e304b6e5f48170699ffc625c5
divawidia <wiartha2001@gmail.com>
2025-02-25 00:00:00 +0000

    feat (AdminAuthController): add showLoginForm methods

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AdminAuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return Inertia::render('Admin/Auth/Login');
+    }
 }
