08ee37850c02f07cadeb24283524a49c7c266e71
divawidia <wiartha2001@gmail.com>
2025-02-26 00:00:00 +0000

    refactor (admin product index): add ProductList vue page

--- resources/js/Pages/Admin/Products/Index.vue
+++ resources/js/Pages/Admin/Products/Index.vue
@@ -1,18 +1,21 @@
-<template>
-    <AdminLayout>
-        <main class="p-4 md:ml-64 h-auto pt-20">
-
-        </main>
-    </AdminLayout>
-</template>
-
 <script setup>
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
 import AdminLayout from "@/Pages/Admin/Component/AdminLayout.vue";
+import ProductLists from "@/Pages/Admin/Products/ProductLists.vue";
 
 // initialize components based on data attribute selectors
 onMounted(() => {
     initFlowbite();
 })
 </script>
+
+<template>
+    <AdminLayout>
+        <ProductLists>
+
+        </ProductLists>
+    </AdminLayout>
+</template>
+
+
