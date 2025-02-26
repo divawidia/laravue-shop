d4d0715654161f18f2dafc3115d5930b12f74ca3
divawidia <wiartha2001@gmail.com>
2025-02-26 00:00:00 +0000

    feat (admin main layout): add main tags

--- resources/js/Pages/Admin/Component/AdminLayout.vue
+++ resources/js/Pages/Admin/Component/AdminLayout.vue
@@ -19,6 +19,8 @@ onMounted(() => {
         <Sidebar></Sidebar>
 
         <!-- Main -->
-        <slot/>
+        <main class="p-4 md:ml-64 h-auto pt-20">
+            <slot/>
+        </main>
     </div>
 </template>
