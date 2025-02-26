481e3030c58f9f62edbfd569a5ecd07d8c36b2f6
divawidia <wiartha2001@gmail.com>
2025-02-26 00:00:00 +0000

    feat (sidebar layout): update dashboard menu

--- resources/js/Pages/Admin/Component/Sidebar.vue
+++ resources/js/Pages/Admin/Component/Sidebar.vue
@@ -35,8 +35,8 @@
             </form>
             <ul class="space-y-2">
                 <li>
-                    <a
-                        href="#"
+                    <Link
+                        :href="route('admin.dashboard')"
                         class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                     >
                         <svg
@@ -49,8 +49,8 @@
                             <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
                             <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
                         </svg>
-                        <span class="ml-3">Overview</span>
-                    </a>
+                        <span class="ml-3">Dashboard</span>
+                    </Link>
                 </li>
                 <li>
                     <button
