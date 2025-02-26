1ebf1b99e39e5aeaa70396dbbbb6cb206f1d46b0
divawidia <wiartha2001@gmail.com>
2025-02-26 00:00:00 +0000

    feat (db seeder): add product seeder data

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+            'title' => 'Samsung Galaxy',
+            'slug' => 'samsung-galaxy',
+            'price' => 10000,
+            'quantity' => 5,
+            'category_id' => 1,
+            'brand_id' => 1,
+            'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        ]);
+    }
+}
