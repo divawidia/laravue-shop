524a0423e68d76a476441f8bf8f0e5a40e371785
divawidia <wiartha2001@gmail.com>
2025-02-26 00:00:00 +0000

    refactor (admin sidebar layout): add products menu

--- resources/js/Pages/Admin/Component/Sidebar.vue
+++ resources/js/Pages/Admin/Component/Sidebar.vue
@@ -91,11 +91,10 @@
                     </button>
                     <ul id="dropdown-pages" class="hidden py-2 space-y-2">
                         <li>
-                            <Link
-                                :href="route('admin.products.index')"
-                                class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                            >Products</Link
-                            >
+                            <Link :href="route('admin.products.index')"
+                                class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                Products
+                            </Link>
                         </li>
                         <li>
                             <a
@@ -580,6 +579,6 @@
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
+import {Link} from "@inertiajs/vue3";
 
 </script>
