cb223069156f45676035eceaee7c2a2a1f689a94
divawidia <wiartha2001@gmail.com>
2025-02-27 00:00:00 +0000

    style (admin layout): update layout height

--- resources/js/Pages/Admin/Component/AdminLayout.vue
+++ resources/js/Pages/Admin/Component/AdminLayout.vue
@@ -4,14 +4,13 @@ import { initFlowbite } from 'flowbite'
 import Navbar from "@/Pages/Admin/Component/Navbar.vue";
 import Sidebar from "@/Pages/Admin/Component/Sidebar.vue";
 
-// initialize components based on data attribute selectors
 onMounted(() => {
     initFlowbite();
 })
 </script>
 
 <template>
-    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <div class="antialiased  bg-gray-50 dark:bg-gray-900">
         <!-- Navbar -->
         <Navbar></Navbar>
 
@@ -19,7 +18,7 @@ onMounted(() => {
         <Sidebar></Sidebar>
 
         <!-- Main -->
-        <main class="p-4 md:ml-64 h-auto pt-20">
+        <main class="p-4 md:ml-64 h-full pt-20">
             <slot/>
         </main>
     </div>
