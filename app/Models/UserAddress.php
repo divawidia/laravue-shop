7443f55c1be33c386e24e9ed2fb4df1af1144ded
divawidia <wiartha2001@gmail.com>
2025-02-27 00:00:00 +0000

    feat (user address model): add fillable and relation methods

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserAddress extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'zipcode',
+        'isMain',
+        'country_code',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
