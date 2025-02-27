a046f430ab8367211121b189f0482dc527eb2c50
divawidia <wiartha2001@gmail.com>
2025-02-27 00:00:00 +0000

    feat (brand model): add fillable and relation methods

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Brand extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($brand) {
+            $brand->slug = Str::slug($brand->name);
+        });
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
