885aaf30f857dc7dcfa71780e2d127006eb0df2f
divawidia <wiartha2001@gmail.com>
2025-02-27 00:00:00 +0000

    feat (order model): add fillable and relation methods

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'total_price',
+        'status',
+        'session_id',
+        'user_address_id',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function userAddress()
+    {
+        return $this->belongsTo(UserAddress::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
 }
