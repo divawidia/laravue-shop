f242431b0ff64051d38abf856ec9ae32c83bda58
divawidia <wiartha2001@gmail.com>
2025-02-27 00:00:00 +0000

    feat (payment model): add fillable and relation methods

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'created_by',
+        'deleted_by',
+        'amount',
+        'status',
+        'type',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function deleter()
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
 }
